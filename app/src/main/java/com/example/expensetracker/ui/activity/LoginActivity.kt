930b529bc17990dcabf02ed202dad2dbedf8ee13
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-01-08 09:05:16 +0545

    Authentication

--- app/src/main/java/com/example/expensetracker/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/expensetracker/ui/activity/LoginActivity.kt
@@ -10,6 +10,7 @@ import com.example.expensetracker.R
 import com.example.expensetracker.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +25,6 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/expensetracker/ui/activity/RegistrationActivity.kt
+++ app/src/main/java/com/example/expensetracker/ui/activity/RegistrationActivity.kt
@@ -1,17 +1,23 @@
 package com.example.expensetracker.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.expensetracker.R
-import com.example.expensetracker.databinding.ActivityLoginBinding
 import com.example.expensetracker.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityRegistrationBinding
+
+    lateinit var auth : FirebaseAuth
+
+    lateinit var name : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,12 +25,28 @@ class RegistrationActivity : AppCompatActivity() {
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
         binding.signUp.setOnClickListener{
-            val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
-            startActivity(intent)
+            var email = binding.registerEmail.text.toString()
+            var password = binding.registerPassword.text.toString()
+
+            auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Toast.makeText(
+                            this@RegistrationActivity,
+                            "Registration success", Toast.LENGTH_LONG
+                        ).show()
+                    } else{
+                        Toast.makeText(
+                            this@RegistrationActivity,
+                            it.exception?.message,Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
         }
 
-        setContentView(R.layout.activity_registration)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
